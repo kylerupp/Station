2dc1d13612121a2affebcccfdaffeda150bc132b
Kyle Rupp <kyle.rupp838@gmail.com>
2020-05-04 19:35:44 -0600

    Add main to repo

---
+++ src/station/Main.java
@@ -0,0 +1,18 @@
+package station;
+
+/**
+ * (basic description of the program or class)
+ * 
+ * Completion time: (estimation of hours spent on this program)
+ *
+ * @author kylerupp
+ * @version May 4, 2020
+ * @since May 4, 2020
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
